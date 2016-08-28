fba98805e64b509812fb24a4ae97d661dcefeb40
vphoenix1972 <vphoenix1972@gmail.com>
2016-08-28 09:34:46 +0300

    Added autofac

--- MyLanguagePalService/Global.asax.cs
+++ MyLanguagePalService/Global.asax.cs
@@ -3,6 +3,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
 using NLog;
 
 namespace MyLanguagePalService
@@ -13,6 +15,18 @@ namespace MyLanguagePalService
 
         protected void Application_Start()
         {
+            // *** Configure Autofac ***
+            var builder = new ContainerBuilder();
+
+            // Register your MVC controllers.
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+
+            // *** Configure MVC ***
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
