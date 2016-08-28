4bb5b0b5b78b270013d55fdebb20aac5e1c55fdc
vphoenix1972 <vphoenix1972@gmail.com>
2016-08-28 11:32:56 +0300

    Added db debug initializer

---
+++ MyLanguagePalService/DAL/DebugInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace MyLanguagePalService.DAL
+{
+    public class DebugInitializer : DropCreateDatabaseIfModelChanges<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            var languages = new List<Language>
+            {
+                new Language() { Name = "English" },
+                new Language() { Name = "Русский" }
+            };
+            languages.ForEach(language => context.Languages.Add(language));
+            context.SaveChanges();
+        }
+    }
+}
