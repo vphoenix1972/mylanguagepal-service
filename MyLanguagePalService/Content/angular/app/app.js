a2205447e31c9c5dba7fdce6771aaf91ec4f6a3f
vphoenix1972 <vphoenix1972@gmail.com>
2016-09-15 10:16:33 +0300

    Added angular basic routing

---
+++ MyLanguagePalService/Content/angular/app/app.js
@@ -0,0 +1,47 @@
+﻿var app = angular.module("myLanguagePal", ["ngRoute"]);
+
+// Configure routes
+app.config(function ($routeProvider) {
+    $routeProvider
+
+        // GET /
+        .when("/", {
+            templateUrl: "app/components/home/home.html",
+            controller: "mainController"
+        })
+
+        // GET /phrases
+        .when("/phrases", {
+            templateUrl: "app/components/phrases/phrases.html",
+            controller: "phrasesController"
+        })
+
+        // GET /languages
+        .when("/languages", {
+            templateUrl: "app/components/languages/languages.html",
+            controller: "languagesController"
+        })
+
+        // GET /about
+        .when("/about", {
+            templateUrl: "app/components/about/about.html",
+            controller: "aboutController"
+        });
+});
+
+// Create the controller and inject Angular's $scope
+app.controller("mainController", function ($scope) {
+    
+});
+
+app.controller("phrasesController", function ($scope) {
+
+});
+
+app.controller("languagesController", function ($scope) {
+
+});
+
+app.controller("aboutController", function ($scope) {
+    
+});
