a6f015fdc7999ab52d2baa0b3d9daca2ee05a920
vphoenix1972 <vphoenix1972@gmail.com>
2016-11-04 20:35:59 +0300

    Added ng-route + angular animation

--- MyLanguagePalService/App_Start/BundleConfig.cs
+++ MyLanguagePalService/App_Start/BundleConfig.cs
@@ -15,29 +15,41 @@ namespace MyLanguagePalService
             bundles.Add(new ScriptBundle("~/deps/modernizr-js").Include(
                         "~/Content/deps/modernizr-*"));
 
-            // Jquery
-            bundles.Add(new ScriptBundle("~/deps/jquery-js").Include(
-                "~/Content/deps/jquery/jquery-3.1.0.js"));
-
-            // Bootstrap
-            bundles.Add(new StyleBundle("~/deps/bootstrap-css").Include(
-                      "~/Content/deps/bootstrap/css/bootstrap.css"));
-            bundles.Add(new ScriptBundle("~/deps/bootstrap-js").Include(
-                      "~/Content/deps/bootstrap/js/bootstrap.js",
-                      "~/Content/deps/respond.js"));
-
             // *** Shared bundles ***
-            bundles.Add(new StyleBundle("~/shared/shared-css").Include(
-                "~/Content/shared/shared.css"));
 
             // *** Site bundles ***
             bundles.Add(new StyleBundle("~/site/site-css").Include(
+                "~/Content/deps/bootstrap/css/bootstrap.css",
+                "~/Content/shared/shared.css",
                 "~/Content/site/site.css"));
 
+            bundles.Add(new ScriptBundle("~/site/site-js").Include(
+                "~/Content/deps/jquery/jquery-3.1.0.js",
+                "~/Content/deps/bootstrap/js/bootstrap.js",
+                "~/Content/deps/respond.js"));
+
             // *** App bundles ***
+            bundles.Add(new StyleBundle("~/app/app-css").Include(
+                "~/Content/deps/bootstrap/css/bootstrap.css",
+                "~/Content/shared/shared.css",
+                "~/Content/app/shared/app.css"));
+
+            bundles.Add(new ScriptBundle("~/app/app-js").Include(
+                // Deps
+                "~/Content/deps/jquery/jquery-3.1.0.js",
+                "~/Content/deps/bootstrap/js/bootstrap.js",
+                "~/Content/deps/respond.js",
+                "~/Content/deps/angular/angular.js",
+                "~/Content/deps/angular/angular-route.js",
+                "~/Content/deps/angular/angular-animate.js",
+                "~/Content/deps/angular/angular-touch.js",
+                "~/Content/deps/angular/ui-bootstrap-tpls-2.2.0.js",
+                // App scripts
+                "~/Content/app/app.js"));
 
             // *** Other bundles ***
             bundles.Add(new StyleBundle("~/other/other-css").Include(
+                "~/Content/shared/shared.css",
                 "~/Content/other/other.css"));
         }
     }
--- MyLanguagePalService/Content/app/app.js
+++ MyLanguagePalService/Content/app/app.js
@@ -1,1 +1,17 @@
-﻿
+﻿var app = angular.module('app', ['ui.bootstrap', 'ngRoute', 'ngAnimate']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl: '/Content/app/pages/about/about.html'
+    })
+    .when('/about', {
+        templateUrl: '/Content/app/pages/about/about.html'
+    })
+    .when('/phrases', {
+        templateUrl: '/Content/app/pages/phrases/phrases.html'
+    })
+    .when('/languages', {
+        templateUrl: '/Content/app/pages/languages/languages.html'
+    });
+});
