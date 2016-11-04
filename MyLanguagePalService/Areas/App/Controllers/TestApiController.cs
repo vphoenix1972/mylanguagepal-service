f772f437eca7fb77b0d4109fe67f753e5cd7ec54
vphoenix1972 <vphoenix1972@gmail.com>
2016-11-04 19:10:08 +0300

    Added web api

---
+++ MyLanguagePalService/Areas/App/Controllers/TestApiController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace MyLanguagePalService.Areas.App.Controllers
+{
+    public class TestApiController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new[] { "value1", "value2" };
+        }
+
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
--- MyLanguagePalService/Global.asax.cs
+++ MyLanguagePalService/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -35,6 +36,7 @@ namespace MyLanguagePalService
 
                 // *** Configure MVC ***
                 AreaRegistration.RegisterAllAreas();
+                GlobalConfiguration.Configure(WebApiConfig.Register);
                 RouteConfig.RegisterRoutes(RouteTable.Routes);
                 BundleConfig.RegisterBundles(BundleTable.Bundles);
             }
