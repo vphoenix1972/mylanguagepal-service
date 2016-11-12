6ad941edfbdc78836dd7c641866611ebd24eb138
vphoenix1972 <vphoenix1972@gmail.com>
2016-11-12 17:42:42 +0300

    Small refactoring

--- MyLanguagePalService/Content/app/services/connector/connectorService.js
+++ MyLanguagePalService/Content/app/services/connector/connectorService.js
@@ -8,24 +8,29 @@ ConnectorService.prototype.getLanguages = function () {
     /// Gets the language list.
     /// Returns a promise:
     /// resolve(languages): Array of languages.
+    /// reject(error): Network or Http error
     /// </summary>
 
-    return this._restService.get('/api/languages');
+    return this._getDataAndHandleAllErrors('/api/languages');
 }
 
 ConnectorService.prototype.getLanguage = function (id) {
     /// <summary>
-    /// Gets the language list.
+    /// Gets the language with specified id.
     /// Returns a promise:
-    /// resolve(languages): Array of languages.
+    /// resolve(language): Language object.
+    /// reject(error): Network or Http error
     /// </summary>
 
-    return this._getAndHandleAllErrors('/api/languages/' + id);
+    return this._getDataAndHandleAllErrors('/api/languages/' + id);
 }
 
-ConnectorService.prototype._getAndHandleAllErrors = function (url) {
+ConnectorService.prototype._getDataAndHandleAllErrors = function (url) {
     var self = this;
-    return self._restService.get(url).catch(
+    return self._restService.get(url).then(
+        function(response) {
+            return response.data;
+        },
         function (response) {
             return self._handleError(response);
         });
--- MyLanguagePalService/Content/app/services/languagesService.js
+++ MyLanguagePalService/Content/app/services/languagesService.js
@@ -4,27 +4,11 @@
 }
 
 LanguagesService.prototype.getLanguages = function () {
-    /// <summary>
-    /// Gets the language list.
-    /// Returns a promise:
-    /// resolve(languages): Array of languages.
-    /// </summary>
-
-    return this._connector.getLanguages().then(function (response) {
-        return response.data;
-    });
+    return this._connector.getLanguages();
 }
 
 LanguagesService.prototype.getLanguage = function (id) {
-    /// <summary>
-    /// Gets the language with the specified id.
-    /// Returns a promise:
-    /// resolve(language): Returns the language or 'undefined' if language was not found.
-    /// </summary>
-
     id = this._utils.parseIntOrThrow(id, 'id');
 
-    return this._connector.getLanguage(id).then(function (response) {
-        return response.data;
-    });
+    return this._connector.getLanguage(id);
 }
