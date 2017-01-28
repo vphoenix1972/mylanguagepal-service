c31a608a711c2fe2b563b67dc4e22a14455c97d1
vphoenix1972 <vphoenix1972@gmail.com>
2017-01-28 17:17:58 +0300

    Added language selection box for phrase edit
    Conflicts:
    	MyLanguagePalService/Scripts/_references.js

---
+++ MyLanguagePalService/Content/app/pages/shared/directives/mlpElement.js
@@ -0,0 +1,25 @@
+﻿MlpElement = (function () {
+    'use strict';
+
+    function MlpElement() {
+
+    }
+
+    MlpElement.prototype.isDisabled = function () {
+        var self = this;
+
+        // ReSharper disable PossiblyUnassignedProperty
+        if (!angular.isObject(self.mlpDisabled))
+            return false;
+
+        return self.mlpDisabled.value();
+        // ReSharper restore PossiblyUnassignedProperty
+    }
+
+    /* Private */
+
+
+    MlpElement.$inject = [];
+
+    return MlpElement;
+})();
